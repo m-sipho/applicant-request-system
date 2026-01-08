726226ed454dbacabe26b9668a09ca274619e8bd
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-08 22:17:28 +0200

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
-
+import { Routes, Route, Navigate } from "react-router-dom"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path={"login"} element={<LoginPage />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   )
 }
 
