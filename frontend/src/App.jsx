caca700b322217aa0999bf7b508dcfa36256c662
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-09 10:58:25 +0200

    Implement the register page and render it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 function App() {
   return (
     <Routes>
       <Route path={"login"} element={<LoginPage />} />
+      <Route path={"register"} element={<RegisterPage />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,54 @@
+import { UserPlus, User, Mail, ArrowRight, Lock } from "lucide-react"
+import { Link } from "react-router-dom"
+
+function RegisterPage() {
+    return (
+        <div className="overall-container">
+            <div className="form-container">
+                <div  className="lock">
+                    <UserPlus size={50}/>
+                </div>
+
+                <div className="sub-header">
+                    <h2>Create Account</h2>
+                    <p className="muted">Join and get started today.</p>
+                </div>
+
+                <form>
+                    <div className="input-wrapper">
+                        <label>Full Name</label>
+                        <div>
+                            <User className="input-icon" />
+                            <input type="text" placeholder="Itachi Uchiha" required/>
+                        </div>
+                    </div>
+                    <div className="input-wrapper">
+                        <label>Email</label>
+                        <div>
+                            <Mail className="input-icon" />
+                            <input type="text" placeholder="your@example.com" required/>
+                        </div>
+                    </div>
+                    <div className="input-wrapper">
+                        <label>Password</label>
+                        <div>
+                            <Lock className="input-icon" />
+                            <input type="password" placeholder="Password" required/>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn-primary">
+                        <span>Sign Up</span>
+                        <ArrowRight className="arrow-right" />
+                    </button>
+                    <div className="line">
+                        <span></span>
+                    </div>
+                </form>
+
+                <div className="muted">Already have an account? <Link to="/login" className="create-account">Log in</Link></div>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterPage
