d6cccd6e6aa145833a29f41a40a894925eef34bc
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-09 16:16:52 +0200

    Update the login page with demo credentials and UI improvements

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,67 @@
-import { Lock, Mail, ArrowRight } from "lucide-react"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Lock, Mail, ArrowRight, AlertCircle, LoaderCircle, ShieldUser, ContactRound, UserRound } from "lucide-react"
+import { Link, useNavigate } from "react-router-dom"
+import { loginUser } from "../services/api"
 
 function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    // Checks if the user has already logged in
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        if (token) {
+            navigate("/dashboard")
+        }
+    }, [navigate]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        // Start loading
+        setLoading(true);
+        setError("");
+
+        try {
+            const data = await loginUser(email, password);
+
+            if (data.access_token) {
+                sessionStorage.setItem("token", data.access_token);
+                navigate("/dashboard");
+            } else {
+                setError(data.detail || "Invalid credentials");
+            }
+        } catch (err) {
+            setError(err.message || "Login Failed")
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    function applicant() {
+        setEmail("applicant@gmail.com")
+        setPassword("applicant");
+    }
+
+    function staff() {
+        setEmail("staff@system.com");
+        setPassword("staff");
+    }
+
+    function admin() {
+        setEmail("admin@gmail.com");
+        setPassword("admin");
+    }
+
     return (
         <div className="overall-container">
             <div className="form-container">
                 <div  className="lock">
-                    <Lock size={50}/>
+                    <Lock size={30}/>
                 </div>
 
                 <div className="sub-header">
@@ -14,46 +69,59 @@ function LoginPage() {
                     <p className="muted">Please sign in to your account.</p>
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-wrapper">
                         <label>Email</label>
                         <div>
                             <Mail className="input-icon" />
-                            <input type="text" placeholder="your@example.com" required/>
+                            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="your@example.com" required/>
                         </div>
                     </div>
                     <div className="input-wrapper">
                         <label>Password</label>
                         <div>
                             <Lock className="input-icon" />
-                            <input type="password" placeholder="Password" required/>
+                            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" required/>
                         </div>
                     </div>
-                    <button type="submit" class="btn-primary">
-                        <span>Sign In</span>
-                        <ArrowRight className="arrow-right" />
+
+                    {error && (
+                        <div className="error">
+                            <AlertCircle />
+                            <span>{error}</span>
+                        </div>
+                    )}
+
+                    <button type="submit" disabled={loading} className="btn-primary">
+                        {loading ? (
+                            <>
+                                <LoaderCircle className="animate-spin" />
+                                Logging in...
+                            </>
+                        ) : (
+                            <>
+                                <span>Sign In</span>
+                                <ArrowRight className="arrow-right" />
+                            </>
+                        )}
                     </button>
-                    <div className="line">
-                        <span></span>
-                    </div>
                 </form>
 
                 <div className="muted">Don't have an account? <Link to="/register" className="create-account">Create Account</Link></div>
 
+                <div className="line"></div>
+
                 <div className="credentials-header">
-                    <span className="demo-header muted">Demo Credentials:</span>
+                    <span className="muted demo-header">QUICK DEMO ACCESS</span>
                     <div className="credentials">
-                        <div className="users muted">
-                            <div className="demo">admin@system.com</div>
-                            <div className="">admin</div>
+                        <div className="users muted" onClick={applicant}>
+                            <UserRound /> Applicant
                         </div>
-                        <div className="users muted">
-                            <div className="demo">staff@system.com</div>
-                            <div className="">staff</div>
+                        <div className="users muted" onClick={staff}>
+                            <ContactRound /> Staff
                         </div>
-                        <div className="users muted">
-                            <div className="demo">applicant@gmail.com</div>
-                            <div className="">password</div>
+                        <div className="users muted" onClick={admin}>
+                            <ShieldUser /> Admin
                         </div>
                     </div>
                 </div>
