82f2da311b84cb5a8a22eba59f3956abd89a8aed
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-10 13:07:14 +0200

    Add a dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
+import DashboardPage from "./pages/DashboardPage"
 
 function App() {
   return (
     <Routes>
       <Route path={"login"} element={<LoginPage />} />
       <Route path={"register"} element={<RegisterPage />} />
+      <Route path={"/dashboard"} element={<DashboardPage />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
