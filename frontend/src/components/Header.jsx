abb410e963cc54636f0d9446ae9a164f79c744ea
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-10 19:42:23 +0200

    Implement and style header of every dashboard

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { LogOut } from "lucide-react"
+
+function Header({ role, username }) {
+    return (
+        <header className="header-wrapper">
+            <h2 className="header-title">Dashboard</h2>
+            <div className="header-right">
+                <div className="sub-left">
+                    <div className="username">Username</div>
+                    <div className="role">Applicant</div>
+                </div>
+                <div className="logout-container">
+                    <LogOut className="logout" size={30} ><title>Logout</title></LogOut>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
