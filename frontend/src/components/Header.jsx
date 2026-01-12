d47d68989f88d6cf4fd7ccc8ff860e23cd4e05d8
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-12 22:42:14 +0200

    Dynamically add current user's email and role

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,13 @@
 import { LogOut } from "lucide-react"
 
-function Header({ role, username }) {
+function Header({ user }) {
     return (
         <header className="header-wrapper">
             <h2 className="header-title">Dashboard</h2>
             <div className="header-right">
                 <div className="sub-left">
-                    <div className="username">{username}</div>
-                    <div className="role">{role}</div>
+                    <div className="username">{user.email}</div>
+                    <div className="role">{user.role ? (user.role.charAt(0).toUpperCase() + user.role.slice(1)) : ""}</div>
                 </div>
                 <div className="logout-container">
                     <LogOut className="logout" size={30} ><title>Logout</title></LogOut>
