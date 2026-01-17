186ae31477a9a97dee2cbba0b30fbec6e32c4085
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-17 10:41:44 +0200

    Implement form submission

--- frontend/src/modals/NewRequestModal.jsx
+++ frontend/src/modals/NewRequestModal.jsx
@@ -1,8 +1,24 @@
 import { X } from "lucide-react"
 import { useState } from "react"
 
-function NewRequestModal({ onClose }) {
-    const [requestType, setRequestType] = useState(null);
+function NewRequestModal({ onClose, requestCreation }) {
+    const [requestType, setRequestType] = useState("");
+    const [studentNumber, setStudentNumber] = useState("");
+    const [moduleCode, setModuleCode] = useState("");
+    const [description, setDescription] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        onClose()
+
+        await requestCreation({
+            requestType: requestType,
+            description: description,
+            studentNumber: studentNumber,
+            moduleCode: moduleCode
+        })
+    }
 
     return (
         <div className="modal-layout">
@@ -11,13 +27,13 @@ function NewRequestModal({ onClose }) {
                     <span>Create a request</span>
                     <X className="close" onClick={onClose} />
                 </div>
-                <form className="create-request-form">
+                <form className="create-request-form" onSubmit={handleSubmit}>
                     <div>
                         <label>Request Type</label>
                         <select className="create-select" value={requestType} onChange={e => setRequestType(e.target.value)}>
-                            <option disabled selected value="choose">Choose an option</option>
+                            <option disabled value="">Choose an option</option>
                             <option value="REMARKING SCRIPT">REMARKING SCRIPT</option>
-                            <option value="refund">REFUND REQUEST</option>
+                            <option value="REFUND REQUEST">REFUND REQUEST</option>
                             <option value="AEGROTAT/SPECIAL EXAM">AEGROTAT/SPECIAL EXAM</option>
                         </select>
                     </div>
@@ -25,14 +41,14 @@ function NewRequestModal({ onClose }) {
                     {requestType &&
                         <div>
                             <label>Student Number</label>
-                            <input type="text" />
+                            <input type="text" placeholder="e.g 12345678" required value={studentNumber} onChange={e => setStudentNumber(e.target.value)} />
                         </div>
                     }
 
                     {(requestType === "REMARKING SCRIPT" || requestType === "AEGROTAT/SPECIAL EXAM") &&
                         <div>
                             <label>Module Code</label>
-                            <input type="text" />
+                            <input type="text" placeholder="MAT2613" required value={moduleCode} onChange={e => setModuleCode(e.target.value)} />
                         </div>
                     }
 
@@ -40,7 +56,7 @@ function NewRequestModal({ onClose }) {
                         <>
                             <div>
                                 <label>Description</label>
-                                <textarea name="" id="" cols="50" rows="10"></textarea>
+                                <textarea placeholder="Leave your message here..." required cols="50" rows="10" value={description} onChange={e => setDescription(e.target.value)}></textarea>
                             </div>
                             <div className="btns">
                                 <button type="button" className="cancel-btn" onClick={onClose}>Cancel</button>
