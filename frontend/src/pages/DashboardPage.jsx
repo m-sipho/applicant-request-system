3bfd35cccf380b75c51eac465224b0678457e593
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-17 10:44:32 +0200

    Implement the request creation handler

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,17 +1,20 @@
 import ApplicantDashboard from "./dashboard/applicantDashboard"
-import { allRequests, user } from "../services/api"
+import { allRequests, user, createRequest } from "../services/api"
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
 function DashboardPage() {
     const [requests, setRequests] = useState([])
     const [currentUser, setCurrentUser] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     const navigate = useNavigate();
     
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setLoading(true)
                 const data = await allRequests();
                 const getUser = await user();
 
@@ -35,10 +38,44 @@ function DashboardPage() {
                 setRequests(data);
             } catch (err) {
                 console.error("Failed to fetch requests", err)
+            } finally {
+                setLoading(false);
             }
         };
         fetchData();
     }, [])
+
+    async function handleCreateRequest(formData) {
+        setLoading(true);
+        setError("");
+
+        try {
+            console.log("Sending request...")
+            const data = await createRequest(formData.requestType, formData.description, formData.studentNumber, formData.moduleCode);
+            console.log("Here is the data:", data)
+
+            // Convert time to local
+            const utcTime = data.created_at.replace(' ', 'T') + 'Z';
+            const dateObject = new Date(utcTime);
+            const month = dateObject.toLocaleString("default", {month: "short"});
+            const day = dateObject.getDate();
+            const year = dateObject.getFullYear();
+
+            const hours = String(dateObject.getHours()).padStart(2, '0');
+            const minutes = String(dateObject.getMinutes()).padStart(2, '0');
+
+            data.created_at = `${month} ${day}, ${year} ${hours}:${minutes}`;
+
+            let copyOfRequests = [...requests];
+            copyOfRequests.push(data)
+            setRequests(copyOfRequests)
+        } catch(err) {
+            console.error("API error:", err);
+            setError(err.message || "Failed to create request")
+        } finally {
+            setLoading(false)
+        }
+    }
 
     async function handleLogOut(e) {
         e.preventDefault();
@@ -49,7 +86,7 @@ function DashboardPage() {
 
     return (
         currentUser.role === "applicant" &&
-            <ApplicantDashboard user={currentUser} requests={requests} onLogout={handleLogOut} />
+            <ApplicantDashboard user={currentUser} requests={requests} onLogout={handleLogOut} onCreateRequest={handleCreateRequest} />
     )
 }
 
