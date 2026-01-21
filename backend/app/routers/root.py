cbe20233ee53beed9fc26e301ec65359137ccece
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-22 00:09:32 +0200

    Cleanup the backend folder

--- backend/app/routers/root.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    tags=["Root"]
-)
-
-@router.get("/")
-async def root():
-    return {"message": "Applicant Request System API. Go to /docs for API documentation."}
